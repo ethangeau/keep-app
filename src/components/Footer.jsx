b685eee2a3bdaaf2ef4d11b250a9a9d485cce03e
yebinge <yebing@student.unimelb.edu.au>
2022-02-24 19:45:23 +0800

    Initialize repo

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+    const date = new Date();
+    const year = date.getFullYear();
+
+    return <footer><p>Copyright @{year}</p></footer>;
+}
+
+export default Footer;
